6f4324ddc81dd7d729587e7c1373d00615e18d5d
Isabella <24170045@uttcampus.edu.mx>
2026-07-08 23:37:49 -0600

    Añadí menú interactivo en `Main` para selección de opciones

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int opcion;
+        int op;
+
+        do{
+            System.out.println("Ingrese una opcion");
+            System.out.println("1.Paquete");
+            System.out.println("2.Reporte final");
+            System.out.println("3.Salir");
+            opcion = sc.nextInt();
+            switch (opcion){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            System.out.println("Desea continuar?");
+            System.out.println("1.Si");
+            System.out.println("2.No");
+            op = sc.nextInt();
+        }while(op == 1);
     }
 }
