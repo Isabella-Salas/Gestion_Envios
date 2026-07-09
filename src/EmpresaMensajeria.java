4b2e885d732f69bb3cd44d50df576bbb28b22c45
Sam <24170137@uttcampus.edu.mx>
2026-07-09 10:27:15 -0600

    Cree el metodo para mostrar los envios y otro que muestre que muestre las ganancias totales.

--- src/EmpresaMensajeria.java
+++ src/EmpresaMensajeria.java
@@ -9,4 +9,11 @@ public class EmpresaMensajeria {
         this.paqueteList.add(paquete);
     }
 
+    public List<Paquete> imprimirListaPaquete(){ return this.paqueteList; }
+
+    public double calcularTotalGanancias() {
+        double totalAcumulado = 0.0;
+        for (Paquete paquete: this.paqueteList){ totalAcumulado += paquete.obtenerCostoEnvio();}
+        return totalAcumulado;
+    }
 }
